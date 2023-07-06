1cdcae5dfa0c8836a5d1530b13d05a869836218a
cosascarlos13 <cosascarlos13@gmail.com>
2023-07-06 11:18:30 -0400

    Add project

---
+++ js/about.5337095b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgo_vuejs"]=self["webpackChunkgo_vuejs"]||[]).push([[443],{782:function(n,u,e){e.r(u),e.d(u,{default:function(){return l}});var s=e(252);const t={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function c(n,u){return(0,s.wg)(),(0,s.iD)("div",t,r)}var o=e(744);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.5337095b.js.map
